66448ccb5366783a6b6c8bed5aa038a4dbe1e305
Lucas Val <lucas.f.val@gmail.com>
2020-07-17 17:41:26 -0300

    Chess Position Created

---
+++ jogo-xadrez/Chess/ChessPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Board;
+
+namespace Chess
+{
+    class ChessPosition
+    {
+        public char posColumn { get; set; }
+        public int posLine { get; set; }
+
+        public ChessPosition(char posColumn, int posLine)
+        {
+            this.posColumn = posColumn;
+            this.posLine = posLine;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - posLine, posColumn - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + posColumn + posLine;
+        }
+    }
+}
--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -9,19 +9,9 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Boards board = new Boards(8, 8);
-                board.putPiece(new Rook(board, Color.Black), new Position(0, 0)); //Put a Rook on 0,0
-                board.putPiece(new Rook(board, Color.Black), new Position(1, 3)); //Put a Rook on 1,3
-                board.putPiece(new King(board, Color.Black), new Position(1, 9)); //Put a King on 2,4
-                board.putPiece(new King(board, Color.Black), new Position(2, 9)); //Put a King on 2,4
-                Screen.PrintBoard(board);
-            }
-            catch(BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition pos = new ChessPosition('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosition());
         }
     }
 }
