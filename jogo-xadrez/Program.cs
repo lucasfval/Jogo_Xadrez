5154c8c29f68858cf192015eea8d9e4ec9568dfd
Lucas Val <lucas.f.val@gmail.com>
2020-07-20 16:15:02 -0300

    Colocada cor da peca preta

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -9,9 +9,19 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            ChessPosition pos = new ChessPosition('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosition());
+            try
+            {
+                Boards board = new Boards(8, 8);
+                board.putPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.putPiece(new Rook(board, Color.Black), new Position(1, 3));
+                board.putPiece(new King(board, Color.Black), new Position(0, 2));
+                board.putPiece(new King(board, Color.White), new Position(3,5));
+                Screen.PrintBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message); 
+            }
         }
     }
 }
